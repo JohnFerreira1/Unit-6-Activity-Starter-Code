4e404e019deaeeaf7298044c8e01302c1d4cf05e
JohnFerreira1 <johnvfbh@gmail.com>
2024-07-18 20:18:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Add JavaScript code here
+const submitRequest = (Event) => {
+  const name = document.getElementById("name");
+  const songName = document.getElementById("song-title")
+  const artistName = document.getElementById("artist-name")
+  event.preventDefault()
+  const songRequest = (`${name.value} has requested ${songName.value} by ${artistName.value}`)
+console.log(songRequest)
+
+}
+
+const form = document.getElementById("request-form");
+
+
+form.addEventListener("submit", submitRequest);
